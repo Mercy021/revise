20eb827731fe2932526c7444fb945ac6002c53c4
mercy021 <kinyanjuimercywangari@gmail.com>
2021-03-17 08:03:11 +0300

    first commit

---
+++ src/arrays.kt
@@ -0,0 +1,47 @@
+import java.util.*
+
+fun main() {
+    var nameArray=arrayOf("Mike","Angela","James")
+
+    var ageArray=arrayOf(25,26,18,19,20,21)
+    for (age in ageArray){
+        var timestwo = age* 2
+        println(timestwo)
+
+averageage()
+
+
+
+    }
+
+}
+fun averageage():Int{
+    var ageArray=arrayOf(25,26,18,19,20,21)
+    var total=ageArray.sum()
+    var size=ageArray.count()
+    var average=total/size
+    println(average)
+    return(average)
+
+
+
+}
+fun age():Int {
+    var ageArray=arrayOf(25,26,18,19,20,21)
+    var order=agedescending.order()
+    var order=
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
